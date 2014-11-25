0c0b5fbf96ac2522ba60cf0f4f1e5e5aa9f03d0e
Bertalan Fodor <foberci@gmail.com>
2014-11-25 13:39:26 +0100

    Default ASP.NET MVC template with local authentication

---
+++ MeetingCatalogue/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeetingCatalogue.MvcApplication" Language="C#" %>
---
+++ MeetingCatalogue/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeetingCatalogue.Startup))]
+namespace MeetingCatalogue
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
